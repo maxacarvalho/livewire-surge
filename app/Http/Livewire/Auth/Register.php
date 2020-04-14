d57eac13b8f2a5e2127cb6cdf02255a7f26818be
Caleb Porzio <calebporzio@gmail.com>
2020-04-14 11:19:02 -0400

    Lesson #2

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Register extends Component
 {
+    public $email = '';
+    public $password = '';
+    public $passwordConfirmation = '';
+
+    public function register()
+    {
+        User::create([
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.auth.register');
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,20 @@
-<div>
-    Hello Registration!
-</div>
+<form wire:submit.prevent="register">
+    <div>
+        <label for="email">email</label>
+        <input wire:model="email" type="text" id="email" name="email">
+    </div>
+
+    <div>
+        <label for="password">password</label>
+        <input wire:model="password" type="password" id="password" name="password">
+    </div>
+
+    <div>
+        <label for="passwordConfirmation">passwordConfirmation</label>
+        <input wire:model="passwordConfirmation" type="password" id="passwordConfirmation" name="passwordConfirmation">
+    </div>
+
+    <div>
+        <input type="submit" value="Register">
+    </div>
+</form>
