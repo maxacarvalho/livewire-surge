8f14d249f41341c6eee4f392c32439fd74ebf59b
Caleb Porzio <calebporzio@gmail.com>
2020-04-29 11:03:55 -0400

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,7 +103,9 @@
         </div>
 
         <main class="flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6" tabindex="0" x-data="" x-init="$el.focus()">
-            @yield('content')
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                @yield('content')
+            </div>
         </main>
     </div>
 </div>
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,11 +1,7 @@
 <div>
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-        <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
-    </div>
+    <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
 
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-        <div class="py-4">
-            <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-        </div>
+    <div class="py-4">
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
     </div>
 </div>
